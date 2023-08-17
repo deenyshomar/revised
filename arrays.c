a908076feb05de34b3803461d49ffa5647f5e43a
deenyshomar <deenyshomar@gmail.com>
2023-08-18 00:55:49 +0100

    arrays

---
+++ arrays.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * Description: arrays
+ *
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	int i;
+	int a[4];
+	a[0] = 2;
+	a[1] = 3;
+	a[2] = 9;
+	a[3] = 1;
+
+	printf("index 2 is : %d\n", a[2]);
+
+	return(0);
+}
