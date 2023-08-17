7b40c76736ddcb33615c707475a2424086c7be60
deenyshomar <deenyshomar@gmail.com>
2023-08-17 18:41:23 +0100

    passing pointer as argument

---
+++ passing_pointers_as_argument.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * Description: Passing pointers as argument
+ *
+ * main - entry point
+ *
+ * @p: pointer parametre
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int increament(int *p)
+{
+	*p = *p + 1;
+}
+int main(void)
+{
+	int i;
+	i = 10;
+	int *j;
+	j = &i;
+
+	printf("value of i before function call : %d\n", i);
+	increament(j);
+	printf("value of i after function call : %d\n", i);
+	return(0);
+}
