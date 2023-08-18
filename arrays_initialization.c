db54951e4096f776a7011fefcaaa411e3d029ee3
deenyshomar <deenyshomar@gmail.com>
2023-08-18 01:36:34 +0100

    arrays initialization

---
+++ arrays_initialization.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * Description: arrays initialization
+ *
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+	int i;
+	int a[5] = {27, 28, 29, 30, 31};
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+}
