e4902b852e7436025b75be9c95d734c5be2e023a
deenyshomar <deenyshomar@gmail.com>
2023-08-18 01:18:50 +0100

    little modification for confirmation

--- arrays.c
+++ arrays.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	int i;
-	int a[4];
+	int a[5];
 	a[0] = 2;
 	a[1] = 3;
 	a[2] = 9;
 	a[3] = 1;
-
+	a[4] = 4;
 	printf("index 2 is : %d\n", a[2]);
 	printf("size of the array is : %zu\n", sizeof a);
 	printf("number of element of the array is : %zu\n", (sizeof a / sizeof (int)));
