3456c2c1372d438d629cf8a21a49c383e1877aa5
deenyshomar <deenyshomar@gmail.com>
2023-08-25 21:41:53 +0100

    right side pyramid with white spaces

--- pyramid.c
+++ pyramid.c
@@ -12,8 +12,10 @@ int main(void)
 	}while (n < 1 || n > 8);
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j < i ; j++)
 		{
+			putchar(' ');
+
 			putchar('#');
 		}
 		putchar('\n');
