e889f0ec38bd94dfea13feec54d51626b91f0337
deenyshomar <deenyshomar@gmail.com>
2023-09-12 00:21:14 +0100

    my own strlen function

--- string_len.c
+++ string_len.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
-#include<string.h>
+//#include<string.h>
 
+int my_strlen(char *s)
+{
+    int count = 0;
+    while (s[count] != '\0')
+    count++;
+    return count;
+}
 int main(void)
 {
     char *s = "deenyshomar";
 
-    printf("the string length is %zu in bytes\n", strlen(s));
+    printf("the string length is %u in bytes\n", my_strlen(s));
     
 }
