e35064183fd4abfb363567ee5bef5bdb0ffde840
deenyshomar <deenyshomar@gmail.com>
2023-09-13 00:43:10 +0100

    struct again

--- passing_pointer_to_struct.c
+++ passing_pointer_to_struct.c
@@ -16,8 +16,11 @@ int main(void)
 {
     struct car mercedence = {.speed=180, .name="4matic"};
     struct car honda = {"Honda civic", 1234.56, 170};
-    set_price(&mercedence, 6666.6666);
-    set_price(&honda, 44444.44);
-    printf("The car name is %s\nThe car price is %f\nThe car speed is %d\n", honda.name, honda.price, honda.speed);
-    printf("The car name is %s\nThe car price is %f\nThe car speed is %d\n\n", mercedence.name, mercedence.price, mercedence.speed);
+    struct car lexus = {"lexux sport", .speed=200};
+    set_price(&mercedence, 8000000.00);
+    set_price(&honda, 3000000.00);
+    set_price(&lexus, 150000000.00);
+    printf("\nThe car name is %s\nThe car price is %.2f\nThe car speed is %d\n\n", honda.name, honda.price, honda.speed);
+    printf("The car name is %s\nThe car price is %.2f\nThe car speed is %d\n\n", lexus.name, lexus.price, lexus.speed);
+    printf("The car name is %s\nThe car price is %.2f\nThe car speed is %d\n\n", mercedence.name, mercedence.price, mercedence.speed);
 }
