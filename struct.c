92de70a30eba5094a731aa2917f437dc2c199c3e
deenyshomar <deenyshomar@gmail.com>
2023-09-12 04:08:33 +0100

    still another way

--- struct.c
+++ struct.c
@@ -8,7 +8,7 @@ struct car
 };
 int main(void)
 {
-    struct car mercedence = {"4matic", 40999.99, 180};
+    struct car mercedence = {.price=40999.99, .speed=180, .name="4matic"};
     struct car honda = {"Honda civic", 1234.56, 170};
 
     printf("The car name is %s\nThe car price is %f\nThe car speed is %d\n\n", mercedence.name, mercedence.price, mercedence.speed);
