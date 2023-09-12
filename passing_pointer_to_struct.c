2b782b038a0d97cb556ab5068f6aff3c6d93dba4
deenyshomar <deenyshomar@gmail.com>
2023-09-13 00:27:44 +0100

    struct advance level

--- passing_pointer_to_struct.c
+++ passing_pointer_to_struct.c
@@ -14,8 +14,10 @@ void set_price(struct car *c, float new_price)
 
 int main(void)
 {
-    struct car mercedence = {.price=40999.99, .speed=180, .name="4matic"};
+    struct car mercedence = {.speed=180, .name="4matic"};
     struct car honda = {"Honda civic", 1234.56, 170};
-    set_price(&honda, 2727.65);
+    set_price(&mercedence, 6666.6666);
+    set_price(&honda, 44444.44);
     printf("The car name is %s\nThe car price is %f\nThe car speed is %d\n", honda.name, honda.price, honda.speed);
+    printf("The car name is %s\nThe car price is %f\nThe car speed is %d\n\n", mercedence.name, mercedence.price, mercedence.speed);
 }
