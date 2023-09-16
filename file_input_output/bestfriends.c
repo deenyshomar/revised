2ae5a26468df500e894661697e1b5c2f3347e87a
deenyshomar <deenyshomar@gmail.com>
2023-09-16 09:24:19 +0100

    formatted input

---
+++ file_input_output/bestfriends.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void)
+{
+    FILE *fp;
+    char Name[1023];
+    float DateOfBirth;
+    int YearWeMet;
+    int linecount = 0;
+
+    fp = fopen("bestfriends.txt", "r");
+
+    while(fscanf(fp, "%s %f %d", Name, &DateOfBirth, &YearWeMet) != EOF)
+    {
+        ++linecount;
+        printf("%d : Name : %s | Date of Birth : %.2f | Year we met : %d\n", linecount, Name, DateOfBirth, YearWeMet);
+    }
+    fclose(fp);
+}
